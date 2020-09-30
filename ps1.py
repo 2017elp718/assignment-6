839163f62bcfdff2a5769d44d03abb1babbaa5dd
PramodSaraswat <37214138+PramodSaraswat@users.noreply.github.com>
2020-09-30 22:30:19 +0530

    Update ps1.py

--- ps1.py
+++ ps1.py
@@ -1,3 +1,9 @@
 ###### this is the first .py file ###########
 
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(thistuple[-1])
+print(thistuple[-4:-1])
+
+
 ####### write your code here ##########
